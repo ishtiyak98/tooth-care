97aab29d550c6c83f7e399e762c64ea3030c8a26
ishtiyak98 <rifatishtiyak@gmail.com>
2022-04-16 21:43:41 +0600

    header and components added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { FaTooth } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <>
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Container>
+                <Navbar.Brand href="#home" className='d-flex'><FaTooth size={"1.5em"}></FaTooth><span className='mx-2'>ToothCare</span></Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="home#services">Services</Nav.Link>
+                        <Nav.Link href="home#services">Location</Nav.Link>
+                        <Nav.Link as={Link} to={"/blogs"}>Blogs</Nav.Link>
+                        <Nav.Link as={Link} to={"/about"}>About</Nav.Link>
+                    </Nav>
+                    <Nav>
+                        <Nav.Link as={Link} to={"/signup"}>Signup</Nav.Link>
+                        <Nav.Link as={Link} to={"/Login"}>Login</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </>
+    );
+};
+
+export default Header;
