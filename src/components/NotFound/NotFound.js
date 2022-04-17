c94c8a5335766b4cd34ee297ae206d785431e06c
ishtiyak98 <rifatishtiyak@gmail.com>
2022-04-17 14:05:58 +0600

    404 page modified

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import "./NotFound.css";
 
 const NotFound = () => {
     return (
-        <div>
-            NotFound
+        <div className='not-found d-flex align-items-center justify-content-center'>
+            <div className='text-success error-text px-5 py-5'>
+                <h1 className='text-center'>404 Error</h1>
+                <h2 className='text-center'>Page Not Found</h2>
+            </div>
         </div>
     );
 };
