3677313a77080f5959d67311efd9238e5d26368e
ishtiyak98 <rifatishtiyak@gmail.com>
2022-04-18 01:58:51 +0600

    read-me and about page edited

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React from "react";
 
 const About = () => {
-    return (
-        <div>
-            about
-        </div>
-    );
+  return (
+    <div className="container my-5">
+      <h2 className="text-success text-center mb-5 ">About Me</h2>
+      <div className="w-75 mx-auto shadow p-4 rounded-3">
+        <h3 className="text-center mb-3">Md. Ishtiyak Ali Rifat</h3>
+        <p>
+          My goal is to become a full-stack web developer. I am really working
+          hard to reach my goal. Every day I learn a new concept and practice
+          that concept by doing some small project. In this way, I can easily
+          memorize my old studies too. Currently I am learning React JS. After
+          that, I will start to learn Laravel. Whenever I get some free time, I
+          read programming-related documentation and journals. In order to
+          maintain my time management, I made a daily routine that I try to
+          follow regularly. By doing all this hopefully, I will achieve my goal.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default About;
